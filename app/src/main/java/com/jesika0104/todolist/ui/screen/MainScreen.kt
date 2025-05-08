39628cd285158a29e99173b053c38df0a0c3007a
Jesika Romauli Aritonang <jesikaromauli4@gmail.com>
2025-05-09 06:19:33 +0700

    Menangani keadaan jika list kosong

--- app/src/main/java/com/jesika0104/todolist/ui/screen/MainScreen.kt
+++ app/src/main/java/com/jesika0104/todolist/ui/screen/MainScreen.kt
@@ -16,6 +16,7 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
@@ -52,12 +53,23 @@ fun ScreenContent(modifier: Modifier = Modifier) {
     val viewModel: MainViewModel = viewModel()
     val data = viewModel.data
 
-    LazyColumn(
-        modifier = modifier.fillMaxSize()
+    if (data.isEmpty()) {
+        Column(
+            modifier = modifier.fillMaxSize().padding(16.dp),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text(text = stringResource(id = R.string.task_empty))
+        }
+    }
+    else {
+        LazyColumn(
+            modifier = modifier.fillMaxSize()
     ) {
-        items(data) {
-            TaskItem(task = it)
-            HorizontalDivider()
+            items(data) {
+                TaskItem(task = it)
+                HorizontalDivider()
+            }
         }
     }
 }

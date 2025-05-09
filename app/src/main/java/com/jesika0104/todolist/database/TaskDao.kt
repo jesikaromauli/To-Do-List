14d8a100206ff71ec616fbc6786163b03e55c319
Jesika Romauli Aritonang <jesikaromauli4@gmail.com>
2025-05-09 15:31:25 +0700

    Membuat database dengan room

---
+++ app/src/main/java/com/jesika0104/todolist/database/TaskDao.kt
@@ -0,0 +1,20 @@
+package com.jesika0104.todolist.database
+
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.Query
+import androidx.room.Update
+import com.jesika0104.todolist.model.Task
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface TaskDao {
+    @Insert
+    suspend fun insert(description: Task)
+
+    @Update
+    suspend fun update(description: Task)
+
+    @Query("SELECT * FROM description ORDER BY timestamp DESC")
+    fun getTask(): Flow<List<Task>>
+}
--- app/src/main/java/com/jesika0104/todolist/model/Task.kt
+++ app/src/main/java/com/jesika0104/todolist/model/Task.kt
@@ -1,8 +1,13 @@
 package com.jesika0104.todolist.model
 
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "description")
 data class Task(
-    val id: Long,
+    @PrimaryKey(autoGenerate = true)
+    val id: Long = 0L,
     val title: String,
     val description: String,
-    val timestamp: String,
+    val timestamp: String
 )
